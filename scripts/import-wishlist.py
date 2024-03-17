fb5291efe2f2e5edca4da035db8375cbb05baca3
Elisabeth Robbins <birdsgithub@gmail.com>
2024-03-16 23:09:54 -0400

    wishlist: working return

--- scripts/import-wishlist.py
+++ scripts/import-wishlist.py
@@ -5,8 +5,11 @@ import csv
 conn = sqlite3.connect('../instance/data.db')
 cursor = conn.cursor()
 
+
 # Path to your CSV file
 csv_file_path = './wishlists.csv'
+
+delimiter = [",", "[", "]"]
 
 # Open and read the CSV file
 with open(csv_file_path, 'r', encoding='utf-8') as csv_file:
@@ -18,8 +21,8 @@ with open(csv_file_path, 'r', encoding='utf-8') as csv_file:
     # Insert data into the table
     for row in csv_reader:
         cursor.execute('''
-            INSERT OR IGNORE INTO wishlist_model (WishlistID, Username, ISBN0, ISBN1, ISBN2, ISBN3, ISBN4, ISBN5, ISBN6, ISBN7, ISBN8, ISBN9, ISBN10, ISBN11, ISBN12, ISBN13, ISBN14, ISBN15, ISBN16, ISBN17, ISBN18, ISBN19, ISBN20, ISBN21, ISBN22, ISBN23, ISBN24, ISBN25, ISBN26, ISBN27, ISBN28, ISBN29)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+            INSERT OR IGNORE INTO wishlist_model (WishlistID, Username, ISBNList)
+            VALUES (?, ?, ?);
             ''', row)
 
 # Commit the changes and close the connection

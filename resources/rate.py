31f7c285e3f606b1d2d1f59845af4a82ccbda510
MRosales2003 <158106061+MRosales2003@users.noreply.github.com>
2024-03-28 17:10:03 -0400

    Book rating and commenting (End of Sprint 4) (#21)
    * Second GET call working
    
    2nd call is done
    
    * POST call working, End of Sprint 4
    
    * One last quick change for Sprint 4

--- resources/rate.py
+++ resources/rate.py
@@ -7,16 +7,16 @@ from sqlalchemy.exc import SQLAlchemyError
 from models import RateModel
 from schemas import RatingsSchema
 
-blp = Blueprint("Ratings", __name__, description="Operations on book ratings and comments")
+blp = Blueprint("Ratings", __name__, description="Operations on book ratings")
 
-@blp.route("/ratings")
+@blp.route("/ratings") # return everythomg from every data entry in ratings.csv
 class RatingList(MethodView):
     @blp.response(200, RatingsSchema(many=True))
     def get(self):
         return RateModel.query.all()
     
-@blp.route("/ratings/<string:isbn>")
-class BookListItem(MethodView): # get all comments for one book
+@blp.route("/ratings/<string:isbn>") # get all comments for one book
+class CommentListOneBook(MethodView):
     @blp.response(200, RatingsSchema(many=True))
     def get(self, isbn):
         try:
@@ -28,6 +28,32 @@ class BookListItem(MethodView): # get all comments for one book
 
             # Return comments in JSON format
             return jsonify(comments)
+        except SQLAlchemyError:
+            # Handle any database errors
+            abort(500, message="Database error occurred.")
+
+@blp.route("/averageRating/<string:isbn>") # get average rating for one book
+class AverageRating(MethodView):
+    @blp.response(200, RatingsSchema(many=True))
+    def get(self, isbn):
+        try:
+            # Fetch ratings for the specified ISBN
+            ratings = RateModel.query.filter_by(ISBN=isbn).with_entities(RateModel.Rating).all()
+
+            # Extract ratings from the list of tuples
+            ratings = [rating[0] for rating in ratings]
+            
+            ratingsTotal = 0.0 # add up value of all ratings into this variable
+            ratingsCount = len(ratings) * 1.0 # number of ratings for a book
+            
+            for rating in ratings:
+                ratingsTotal += rating
+
+            # Get the average rating
+            average_rating = ratingsTotal / ratingsCount
+
+            # Return average rating
+            return jsonify(average_rating)
         except SQLAlchemyError:
             # Handle any database errors
             abort(500, message="Database error occurred.")
